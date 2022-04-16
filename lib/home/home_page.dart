034747fe47a93aa400898f8c1ea844eaf275cad1
Felipe Sales <soufeliposales@gmail.com>
2022-04-16 13:46:01 -0300

    adding route to features

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -7,9 +7,28 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('HomePage'),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('bloc/example/');
+              },
+              child: const Text('Bloc Example'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/cubit');
+              },
+              child: const Text('Cubit'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
