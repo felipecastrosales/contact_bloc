df8472ea4d478f406a921ab9367df811d5e209a8
Felipe Sales <soufeliposales@gmail.com>
2022-04-20 08:49:27 -0300

    adding screens

---
+++ lib/features/contacts/contact_register/contact_register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactRegisterPage extends StatelessWidget {
+  const ContactRegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/features/contacts/contact_update/contact_update_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactUpdatePage extends StatelessWidget {
+  const ContactUpdatePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update'),
+      ),
+      body: Container(),
+    );
+  }
+}
